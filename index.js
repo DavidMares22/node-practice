c30426824c774c13f34dd4b1b03887d9cc2994ce
damr <damr18@gmail.com>
2021-02-18 15:10:04 -0600

    get page 1 with promises

--- index.js
+++ index.js
@@ -1,104 +1,47 @@
+const { rejects } = require("assert");
 const https = require("https");
 // https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}
 
 function getMovieTitles(substr) {
   let titles = [];
   let total_pages;
-  
-  
+
+  let request = new Promise((resolve, reject) => {
     https
-    .get(
-      `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}&page=1`,
-      (resp) => {
-        let data = "";
+      .get(
+        `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}&page=1`,
+        (resp) => {
+          let data = "";
 
-        // A chunk of data has been received.
-        resp.on("data", (chunk) => {
-          data += chunk;
-        });
-
-        // The whole response has been received. Print out the result.
-        resp.on("end", () => {
-          // console.log(JSON.parse(data));
-          json = JSON.parse(data);
-          total_pages = json.total_pages;
-          // console.log(total_pages);
-          json.data.map((el) => {
-            titles.push(el.Title);
+          // A chunk of data has been received.
+          resp.on("data", (chunk) => {
+            data += chunk;
           });
-          // console.log(titles.sort());
-        });
-      }
-    )
-    .on("error", (err) => {
-      console.log("Error: " + err.message);
+
+          // The whole response has been received. Print out the result.
+          resp.on("end", () => {
+            json = JSON.parse(data);
+            resolve(json);
+          });
+        }
+      )
+      .on("error", (err) => {
+        console.log("Error: " + err.message);
+        reject(err);
+      });
+  });
+
+  request
+    .then((data) => {
+      data.data.map((el) => {
+        titles.push(el.Title);
+      });
+      console.log(titles);
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  
-
-  
-
-    https
-      .get(
-        `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}&page=2`,
-        (resp) => {
-          let data = "";
-  
-          // A chunk of data has been received.
-          resp.on("data", (chunk) => {
-            data += chunk;
-          });
-  
-          // The whole response has been received. Print out the result.
-          resp.on("end", () => {
-            // console.log(JSON.parse(data));
-            json = JSON.parse(data);
-            total_pages = json.total_pages;
-            // console.log(total_pages);
-            json.data.map((el) => {
-              titles.push(el.Title);
-            });
-            // console.log(titles.sort());
-          });
-        }
-      )
-      .on("error", (err) => {
-        console.log("Error: " + err.message);
-      });
-  
-  
-
-    https
-      .get(
-        `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}&page=3`,
-        (resp) => {
-          let data = "";
-  
-          // A chunk of data has been received.
-          resp.on("data", (chunk) => {
-            data += chunk;
-          });
-  
-          // The whole response has been received. Print out the result.
-          resp.on("end", () => {
-            // console.log(JSON.parse(data));
-            json = JSON.parse(data);
-            total_pages = json.total_pages;
-            // console.log(total_pages);
-            json.data.map((el) => {
-              titles.push(el.Title);
-            });
-            console.log(titles.sort());
-          });
-        }
-      )
-      .on("error", (err) => {
-        console.log("Error: " + err.message);
-      });
-  
- 
-
- 
 }
 
-// getMovieTitles("spiderman");
-getMovieTitles("water");
+getMovieTitles("spiderman");
+// getMovieTitles("water");
