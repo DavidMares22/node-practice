41aa9d74824d4e24bb701249237e0205e15f729b
damr <damr18@gmail.com>
2021-02-18 13:03:09 -0600

    getting pages sequentially

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const https = require("https");
 
 function getMovieTitles(substr) {
   let titles = [];
-
-  https
+  let total_pages;
+  
+  
+    https
     .get(
-      `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}`,
+      `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}&page=1`,
       (resp) => {
         let data = "";
 
@@ -19,16 +21,84 @@ function getMovieTitles(substr) {
         resp.on("end", () => {
           // console.log(JSON.parse(data));
           json = JSON.parse(data);
+          total_pages = json.total_pages;
+          // console.log(total_pages);
           json.data.map((el) => {
             titles.push(el.Title);
           });
-          console.log(titles.sort());
+          // console.log(titles.sort());
         });
       }
     )
     .on("error", (err) => {
       console.log("Error: " + err.message);
     });
+  
+
+  
+
+    https
+      .get(
+        `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}&page=2`,
+        (resp) => {
+          let data = "";
+  
+          // A chunk of data has been received.
+          resp.on("data", (chunk) => {
+            data += chunk;
+          });
+  
+          // The whole response has been received. Print out the result.
+          resp.on("end", () => {
+            // console.log(JSON.parse(data));
+            json = JSON.parse(data);
+            total_pages = json.total_pages;
+            // console.log(total_pages);
+            json.data.map((el) => {
+              titles.push(el.Title);
+            });
+            // console.log(titles.sort());
+          });
+        }
+      )
+      .on("error", (err) => {
+        console.log("Error: " + err.message);
+      });
+  
+  
+
+    https
+      .get(
+        `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}&page=3`,
+        (resp) => {
+          let data = "";
+  
+          // A chunk of data has been received.
+          resp.on("data", (chunk) => {
+            data += chunk;
+          });
+  
+          // The whole response has been received. Print out the result.
+          resp.on("end", () => {
+            // console.log(JSON.parse(data));
+            json = JSON.parse(data);
+            total_pages = json.total_pages;
+            // console.log(total_pages);
+            json.data.map((el) => {
+              titles.push(el.Title);
+            });
+            console.log(titles.sort());
+          });
+        }
+      )
+      .on("error", (err) => {
+        console.log("Error: " + err.message);
+      });
+  
+ 
+
+ 
 }
 
-getMovieTitles("spiderman");
+// getMovieTitles("spiderman");
+getMovieTitles("water");
