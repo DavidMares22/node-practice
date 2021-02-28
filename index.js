eead05f529263ca7705356305146a56aa9401824
damr <damr18@gmail.com>
2021-02-27 20:22:19 -0600

    handlebars

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const express = require('express')
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const app = express();
 
-const app = express()
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
 
-const PORT = process.env.PORT || 3000
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-
-})
-
+  console.log(`Server is running on port ${PORT}`);
+});
