233d5de472b67314b364178c76a3c3e2ee3bd90c
damr <damr18@gmail.com>
2021-03-05 22:13:44 -0600

    use router object

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
+const homeRoutes = require("./routes/home")
+const coursesRoutes = require("./routes/courses")
+const addRoutes = require("./routes/add")
+
 const app = express();
+
 
 app.engine(
   "hbs",
@@ -14,27 +19,13 @@ app.engine(
 app.set("view engine", "hbs");
 
 app.use(express.static("public"));
+app.use('/',homeRoutes)
+app.use('/courses',coursesRoutes)
+app.use('/add',addRoutes)
 
-app.get("/", (req, res) => {
-  res.render("index", {
-    title: "Main page",
-    isHome: true,
-  });
-});
 
-app.get("/add", (req, res) => {
-  res.render("add", {
-    title: "Add course",
-    isAdd: true,
-  });
-});
 
-app.get("/courses", (req, res) => {
-  res.render("courses", {
-    title: "Courses",
-    isCourses: true,
-  });
-});
+ 
 
 const PORT = process.env.PORT || 3000;
 
