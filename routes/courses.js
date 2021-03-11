697ea0aaed050ebe38731106f54d2763983b5f74
damr <damr18@gmail.com>
2021-03-10 18:23:26 -0600

    show detail page with new layout

--- routes/courses.js
+++ routes/courses.js
@@ -1,15 +1,23 @@
-const {Router} = require('express')
-const Course = require("../models/course")
-const router = Router()
+const { Router } = require("express");
+const Course = require("../models/course");
+const router = Router();
 
 router.get("/", async (req, res) => {
-    const courses = await Course.getAll()
-    res.render("courses", {
-      title: "Courses",
-      isCourses: true,
-      courses
-    });
+  const courses = await Course.getAll();
+  res.render("courses", {
+    title: "Courses",
+    isCourses: true,
+    courses,
   });
-  
+});
 
-module.exports = router
+router.get("/:id", async (req, res) => {
+  const course = await Course.getById(req.params.id);
+  res.render("course", {
+    layout:'empty',
+    title: `Course ${course.title}`,
+    course
+  });
+});
+
+module.exports = router;
