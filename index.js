548c84eff6797e3b0d4be79703178eb7e3a0e82a
damr <damr18@gmail.com>
2021-03-21 20:05:23 -0600

    connect to mongo

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const homeRoutes = require("./routes/home");
 const coursesRoutes = require("./routes/courses");
 const addRoutes = require("./routes/add");
 const cardRoutes = require("./routes/card");
-const path = require("path")
-
+const path = require("path");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
+
+
+dotenv.config({ path: "./.env" });
 
 app.engine(
   "hbs",
@@ -28,6 +32,17 @@ app.use("/card", cardRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function start() {
+  try {
+    
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true , useUnifiedTopology: true});
+    // console.log(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
