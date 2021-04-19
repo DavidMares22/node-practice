94738f42f43ad74f3016a5b468ee2ddbbafcbe97
damr <damr18@gmail.com>
2021-04-18 22:30:15 -0500

    flash error messages

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const csrf = require("csurf")
+const flash = require("connect-flash")
 const session = require("express-session");
 const MongoStore = require("connect-mongodb-session")(session)
 const homeRoutes = require("./routes/home");
@@ -51,6 +52,7 @@ app.use(
   })
 );
 app.use(csrf())
+app.use(flash())
 app.use(varMiddleware)
 app.use(userMiddleware)
 
--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,8 @@ router.get("/login", async (req, res) => {
   res.render("auth/login", {
     title: "authorization",
     isLogin: true,
+    loginError:req.flash('loginError'),
+    registerError:req.flash('registerError'),
   });
 });
 
@@ -34,9 +36,11 @@ router.post('/login', async (req, res) => {
           res.redirect('/')
         })
       } else {
+        req.flash('loginError', 'The password is not correct')
         res.redirect('/auth/login#login')
       }
     } else {
+      req.flash('loginError', 'That User does not exists')
       res.redirect('/auth/login#login')
     }
   } catch (e) {
@@ -52,6 +56,7 @@ router.post('/register', async (req, res) => {
     const candidate = await User.findOne({ email })
 
     if (candidate) {
+      req.flash('registerError', 'User with that email already exists')
       res.redirect('/auth/login#register')
     } else {
       const hashPassword = await bcrypt.hash(password, 10)
